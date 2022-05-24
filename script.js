db95492a0cfb42838ab7a4bdfdd1db55390c4954
jacksoze <kamonyojack@gmail.com>
2022-05-24 16:45:39 +0300

    Add a mouseenter event to change div's colors

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function createDivs() {
     const container = document.querySelector('#container');
-    let numberOfDivs = 16;
+    let numberOfDivs = 256;
     for (let i=0; i<numberOfDivs; i++) {
         const squareDiv = document.createElement('div');
-        squareDiv.classList.add('child');
-        squareDiv.textContent='i am a square div'
+        squareDiv.classList.add('squareDiv');
+        
         container.appendChild(squareDiv)
     }
 }
 
 createDivs()
+
+const squareDivs = document.querySelectorAll('.squareDiv')
+squareDivs.forEach(squareDiv =>
+    squareDiv.addEventListener('mouseenter', function(e){
+        this.classList.add('color')
+    }) 
+    )
