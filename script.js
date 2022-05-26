bbeb017cafa2bbd899a154af34f69c034d7497ac
jacksoze <kamonyojack@gmail.com>
2022-05-26 15:03:44 +0300

    Change the no. of divs per side from a propmt

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function createDivs() {
     const container = document.querySelector('#container');
-    let numberOfDivs = 256;
-    for (let i=0; i<numberOfDivs; i++) {
+    let numberOfDivs = parseInt(prompt('how many divs per side?',16));
+    for (let i=0; i<(numberOfDivs*numberOfDivs); i++) {
         const squareDiv = document.createElement('div');
         squareDiv.classList.add('squareDiv');
         
         container.appendChild(squareDiv)
     }
+    const clear = document.createElement('div')
+    clear.classList.add('clear')
+    container.appendChild(clear)
+
+    container.setAttribute('style',`grid-template-columns: repeat(${numberOfDivs}, 2fr) ; grid-template-rows: repeat(${numberOfDivs}, 2fr)`);
+    
 }
 
 createDivs()
